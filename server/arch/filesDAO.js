0d633e94ca502a909543d00f2c4822b445c3f57b
VoskanGrigoryan <voskan.grigoryan.arg@gmail.com>
2021-06-24 21:21:12 -0300

    test

--- server/arch/filesDAO.js
+++ server/arch/filesDAO.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 4000;
 const DB_CONNECTION_URL = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.qb578.mongodb.net/${process.env.DB_NAME}`;
 
 class FileDao {
+    //func init corre bien, y tira los console.log de que se conecto a la db
+    //pero cuando hago una peticion al service me dice que no se puede correr get/post de x cosa
     async init() {
         try {
             mongoose
