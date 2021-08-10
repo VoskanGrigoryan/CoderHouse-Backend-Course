a63d81a8dd772cda900efacb0b57ec6ff271d2b7
VoskanGrigoryan <voskan.grigoryan.arg@gmail.com>
2021-08-10 18:58:19 -0300

    commit

---
+++ server/tests/index.js
@@ -0,0 +1,7 @@
+import { assert } from 'chai'; // Using Assert style
+import { expect } from 'chai'; // Using Expect style
+import { should } from 'chai'; // Using Should style
+
+should();
+expect();
+assert();
